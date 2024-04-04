0dc0d268224aaf0c1887729d5b824df4d5f75be1
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-04-03 17:00:54 -0700

    added readme template to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,56 +1,86 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-// TODO: Create an array of questions for user input
-const questions = [
+// TODO: Create an array of questions for user 
+inquirer.prompt([
     {
-    type:"input",
-    message:"Enter your README title: ",
-    name: "title",
-    },
-    {
-    type:"input",
-    message:"What is the description?",
-    name: "description",
-    },
-    {
-    type:"input",
-    message:"What is the installation instructions?",
-    name: "installation",
-    },
-    {
-    type:"input",
-    message:"How will we use this project?",
-    name: "usage",
-    },
-    {
-    type:"input",
-    message:"Enter the contribution guidelines",
-    name: "contributing",
-    },
-    {
-    type:"input",
-    message:"Enter the test instructions",
-    name: "tests",
-    },
-    {
-    type:"list",
-    choices: ["MIT", "GPL", "MPL"],
-    message:"Which license does your project use?",
-    name:"license"
-    },
-    {
-    type:"input",
-    message:"What is your github username?",
-    name: "github",
-    },
-    {
-    type:"input",
-    message:"What is your email address?",
-    name: "email",
-    },
-    
+        type:"input",
+        message:"Enter your README title: ",
+        name: "title",
+        },
+        {
+        type:"input",
+        message:"What is the description?",
+        name: "description",
+        },
+        {
+        type:"input",
+        message:"What is the installation instructions?",
+        name: "installation",
+        },
+        {
+        type:"input",
+        message:"How will we use this project?",
+        name: "usage",
+        },
+        {
+        type:"input",
+        message:"Enter the contribution guidelines",
+        name: "contributing",
+        },
+        {
+        type:"input",
+        message:"Enter the test instructions",
+        name: "tests",
+        },
+        {
+        type:"list",
+        choices: ["MIT", "GPL", "MPL"],
+        message:"Which license does your project use?",
+        name:"license"
+        },
+        {
+        type:"input",
+        message:"What is your github username?",
+        name: "github",
+        },
+        {
+        type:"input",
+        message:"What is your email address?",
+        name: "email",
+        },
+])
+.then((data) => {
+    console.log(data);
 
-];
+    const readMe = `
+    
+    # ${title}
+
+
+
+    ## description
+    ${description}
+    ## installation
+    ${installation}
+    ## usage
+    ${usage}
+    ## contributing 
+    ${contributing}
+    ## tests 
+    ${tests}
+    ## license
+    ${license} 
+    
+    ## questions
+    
+    for questions contact me at : ${github} or ${email} 
+    
+    `
+console.log(readMe);
+});
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Professional README Generator Starter Code
+# title
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+
+## description
+
+## installation
+
+## usage
+
+## contributing 
+
+## tests 
+
+## license 
+
+## questions
+
+for questions contact me at : github or email 
+
